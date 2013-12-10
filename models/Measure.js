495c300f5ef021a887c78a008931fcac0dc37036
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2013-12-10 14:24:05 +0000

    measure entity

--- models/Measure.js
+++ models/Measure.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mommuge');
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mommuge';
+mongoose.connect(mongoUri);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
